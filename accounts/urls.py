c08ae914ecaf7b49bcee3d6df4650e20b4224818
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-25 19:05:21 +0600

    Добавление приложения accounts

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from accounts.views import login_view, logout_view, RegisterView
+
+urlpatterns =[
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
